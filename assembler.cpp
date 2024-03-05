e7e88261dec657bb4efccaad52fc347fe8b83ade
Jai Anurag Y <y.jaianurag@gmail.com>
2024-03-06 00:43:39 +0530

    Added R format and I format functions

--- assembler.cpp
+++ assembler.cpp
@@ -1,6 +1,75 @@
 #include <bits/stdc++.h>
+#include "lookup.cpp"
 using namespace std;
 #define ll long long int
+
+
+
+
+string Rformat (vector <string> &instruction)
+{
+    // Validate instruction format
+    if (instruction.size() != 4) {
+        return "Invalid instruction format";
+    }
+
+    string opcode_temp = codes_map[instruction[0]][0];
+    string func3_temp = codes_map[instruction[0]][1];
+    string func7_temp = codes_map[instruction[0]][2];
+
+    int opcode = stoi(opcode_temp, nullptr, 16);
+    int func3 = stoi(func3_temp, nullptr, 16);
+    int func7 = stoi(func7_temp, nullptr, 16);
+
+    int rd = stoi(instruction[1].substr(1)); 
+    int rs1 = stoi(instruction[2].substr(1)); 
+    int rs2 = stoi(instruction[3].substr(1)); 
+
+    ll machineCode = (func7 << 25) | (rs2 << 20) | (rs1 << 15) | (func3 << 12) | (rd << 7) | opcode;
+    stringstream ss;
+    ss << hex << "0x" << uppercase << std::setw(8) << std::setfill('0') << machineCode;
+    
+    cout << ss.str()<< endl;
+    return ss.str(); 
+}
+
+
+
+
+
+string Iformat (vector <string> &instruction)
+{
+    // Validate instruction format
+    if (instruction.size() != 4) {
+        return "Invalid instruction format";
+    }
+
+    string opcode_temp = codes_map[instruction[0]][0];
+    string func3_temp = codes_map[instruction[0]][1];
+
+
+    int opcode = stoi(opcode_temp, nullptr, 16);
+    int func3 = stoi(func3_temp, nullptr, 16);
+    int rd = stoi(instruction[1].substr(1)); 
+    int rs1 = stoi(instruction[2].substr(1));
+    int imm;
+    if (instruction[3][0] == '0' && (instruction[3][1] == 'x' || instruction[3][1] == 'X'))
+        imm = stoi(instruction[3].substr(2), nullptr, 16);
+    else
+        imm = stoi(instruction[3]);
+
+
+    ll machineCode = (imm << 20) | (rs1 << 15) | (rs1 << 15) | (func3 << 12) | (rd << 7) | opcode;
+    stringstream ss;
+    ss << hex << "0x" << uppercase << std::setw(8) << std::setfill('0') << machineCode;
+
+
+    cout << ss.str()<< endl;
+    return ss.str();
+}
+
+
+
 
 
 int main() {
