66a8dfd355c033174038afa5a6b82a76cde6c121
dev-rwt <rwt.devansh@gmail.com>
2024-03-05 23:58:44 +0530

    Added parser in main function
    Generates a vector of tokens of each line

--- assembler.cpp
+++ assembler.cpp
@@ -3,8 +3,33 @@ using namespace std;
 #define ll long long int
 
 
-int main()
-{
-    printf("Hello World !");
+int main() {
+    ifstream inputFile("ex.asm");
+    ofstream dataTokenFile("output_token.mc");
+    string line;
+
+    while (getline(inputFile, line)) {
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        stringstream ss(line);
+        vector<string> tokens;
+        string token;
+        int a;
+        while (ss >> token) {
+            a = token.size()-1;
+            if(token[a]==','){
+                token.pop_back();
+            }
+            tokens.push_back(token);
+            dataTokenFile<<token<<endl;
+        }
+     }
+
+     inputFile.close();
+     dataTokenFile.close();
+
     return 0;
 }
+
