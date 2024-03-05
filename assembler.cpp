05fc331c3d8336fcf96466f927b8893311514c83
Jai Anurag Y <y.jaianurag@gmail.com>
2024-03-06 03:54:56 +0530

    Corrected I format and S format for negative immediate values

--- assembler.cpp
+++ assembler.cpp
@@ -56,16 +56,30 @@ string Iformat (vector <string> &instruction)
     int opcode = stoi(opcode_temp, nullptr, 16);
     int func3 = stoi(func3_temp, nullptr, 16);
     int rd = stoi(instruction[1].substr(1));
-    int imm, rs1;
+    int rs1;
+    ll imm;
 
-    
+
     if (!flag)
     {   
         rs1 = stoi(instruction[2].substr(1));
-        if (instruction[3][0] == '0' && (instruction[3][1] == 'x' || instruction[3][1] == 'X'))
+        int flag2 = 0;
+        if (instruction[3][0] == '-'){
+            instruction[3] = instruction[3].substr(1);
+            flag2 = 1;
+        }
+
+        if (instruction[3][0] == '0' && (instruction[3][1] == 'x' || instruction[3][1] == 'X')){
             imm = stoi(instruction[3].substr(2), nullptr, 16);
-        else
+        }
+        else{
             imm = stoi(instruction[3]);
+        }
+        if (flag2) imm *= -1;
+        if (imm<0){
+            imm = (1 << 12) + imm;
+            imm = imm & 0xFFF;
+        }
     }
     else
     {
@@ -75,17 +89,29 @@ string Iformat (vector <string> &instruction)
 
 
         string imm_str = str.substr(0,start_pos);
+        int flag2 = 0;
+        if (imm_str[0] == '-'){
+            imm_str = imm_str.substr(1);
+            flag2 = 1;
+        }
+
         if (imm_str[0] == '0' && (imm_str[1] == 'x' || imm_str[1] == 'X'))
             imm = stoi(imm_str.substr(2), nullptr, 16);
-        else
-            imm = stoi(imm_str);
+        else{
+            imm = stoi(instruction[3]);
+        }
         
+        if (flag2) imm *= -1;
+        if (imm<0){
+            imm = (1 << 12) + imm;
+            imm = imm & 0xFFF;
+        }
 
         rs1 = stoi((str.substr(start_pos+1,end_pos - start_pos -1)).substr(1));
     }
 
 
-    ll machineCode = (imm << 20) | (rs1 << 15) | (rs1 << 15) | (func3 << 12) | (rd << 7) | opcode;
+    ll machineCode = (imm << 20) | (rs1 << 15) | (func3 << 12) | (rd << 7) | opcode;
     stringstream ss;
     ss << hex << "0x" << uppercase << std::setw(8) << std::setfill('0') << machineCode;
 
@@ -110,7 +136,8 @@ string Sformat (vector <string> &instruction)
     int opcode = stoi(opcode_temp, nullptr, 16);
     int func3 = stoi(func3_temp, nullptr, 16);
     int rs2 = stoi(instruction[1].substr(1));
-    int imm, rs1;
+    int rs1;
+    ll imm;
 
 
     string str = instruction[2];
@@ -119,11 +146,27 @@ string Sformat (vector <string> &instruction)
 
 
     string imm_str = str.substr(0,start_pos);
-    if (imm_str[0] == '0' && (imm_str[1] == 'x' || imm_str[1] == 'X'))
-        imm = stoi(imm_str.substr(2), nullptr, 16);
-    else
-        imm = stoi(imm_str);
-    int imm1,imm2;
+    int flag2 = 0;
+    if (imm_str[0] == '-'){
+        imm_str = imm_str.substr(1);
+        flag2 = 1;
+    }
+
+    if (imm_str[0] == '0' && (imm_str[1] == 'x' || imm_str[1] == 'X')){
+        imm = stol(imm_str.substr(2), nullptr, 16);
+    }
+    else{
+        imm = stol(imm_str);
+
+    }
+
+    if (flag2) imm *= -1;
+    if (imm<0){
+        imm = (1 << 12) + imm;
+        imm = imm & 0xFFF;
+    }
+
+    ll imm1,imm2;
     imm1 = (imm>>5) & 127;
     imm2 = (imm) & 31;
 
